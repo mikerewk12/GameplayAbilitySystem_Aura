5540123f965987ad4da9293a9020ccc8dc8d5625
mikerewk12 <36935175+mikerewk12@users.noreply.github.com>
2023-12-27 06:41:04 -0800

    Base Character

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Tentacle 51, LLC
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Tentacle 51, LLC
+
+#pragma once
+
+#include "CoreMinimal.h"
+
