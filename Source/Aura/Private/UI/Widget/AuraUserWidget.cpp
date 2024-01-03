72484d32b8b63f1ab69090c50157553561acb17f
mikerewk12 <36935175+mikerewk12@users.noreply.github.com>
2024-01-02 19:32:45 -0800

    Section 5-29

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Tentacle 51, LLC
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Tentacle 51, LLC
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
